655af5eef78b311b28384b3ba50455ec79a14912
Ihor Panchenko <ihor.panchenko@tul.cz>
2024-10-10 13:53:58 +0300

    nova1

--- IS projekty/001-první-program-vypis-rady/Program.cs
+++ IS projekty/001-první-program-vypis-rady/Program.cs
@@ -15,7 +15,42 @@ class Program{
            Console.WriteLine("************************\n\n");
            Console.WriteLine();
 
-           // Vstup do uživatele - TO-DO
+           // Vstup do uživatele - špatná varianta
+           //Console.Write("Zadejte první číslo řady: ");
+           //int first = int.Parse(Console.ReadLine());
+        
+           // Vstup do uživatele - lepší varianta
+           Console.Write("Zadejte první číslo řady (celé číslo): ");
+           int  first;
+           while(int.TryParse(Console.ReadLine(), out first)){
+                Console.Write("Nezadali jsme celé číslo. Zadejte znovu první číslo řády(celé číslo): ");
+           } 
+
+           Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+           int  first;
+           while(int.TryParse(Console.ReadLine(), out first)){
+
+                Console.Write("Nezadali jsme celé číslo. Zadejte znovu poslední číslo řády(celé číslo): ");
+           }
+
+           Console.Write("Zadejte diference (celé číslo): ");
+           int  first;
+           while(int.TryParse(Console.ReadLine(), out first)){
+
+                Console.Write("Nezadali jsme celé číslo. Zadejte diference (celé číslo): ");
+           }
+
+           //Vypis uživatelského vstupu 
+        
+           Console.WriteLine();
+           Console.WriteLine("===========");
+           Console.WriteLine("První číslo: {0}", again);
+           Console.WriteLine("Poslední číslo {0}", last);
+           Console.WriteLine("Diference číslo {0}", step);
+           Console.WriteLine();
+           Console.WriteLine();
+
+
 
            // Logika pro výpis řády - TO-DO
 
--- IS projekty/001-první-program-vypis-rady/obj/Debug/net8.0/001-první-program-vypis-rady.AssemblyInfo.cs
+++ IS projekty/001-první-program-vypis-rady/obj/Debug/net8.0/001-první-program-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001-první-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092f7fb7d3a2290486bdfc544b9f371686639e2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096ff4c0a2973d340c57abc20617a3d80ca66015")]
 [assembly: System.Reflection.AssemblyProductAttribute("001-první-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001-první-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
