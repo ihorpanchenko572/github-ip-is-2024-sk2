096ff4c0a2973d340c57abc20617a3d80ca66015
Ihor Panchenko <ihor.panchenko@tul.cz>
2024-10-10 13:33:20 +0300

    nova

---
+++ IS projekty/001-první-program-vypis-rady/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+                
+         // chci, aby se program opakoval po stisku klávesy "a"       
+        string again = "a";
+        while(again == "a") {
+           Console.Clear();
+           Console.WriteLine("***********");
+           Console.WriteLine("***** Výpis řády ******");
+           Console.WriteLine("***************");
+           Console.WriteLine("********* Ihor Panchenko *********");
+           Console.WriteLine("************************\n\n");
+           Console.WriteLine();
+
+           // Vstup do uživatele - TO-DO
+
+           // Logika pro výpis řády - TO-DO
+
+
+           // Opaskování programu 
+           Console.WriteLine("Pro opákování programu stiskněte klávesu a");
+           again = Console.ReadLine();
+        }
+
+
+    }
+}
