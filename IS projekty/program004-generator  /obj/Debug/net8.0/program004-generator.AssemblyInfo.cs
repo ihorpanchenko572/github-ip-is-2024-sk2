ff70f6374809723b877c1f7694269d9965215a2b
Ihor Panchenko <ihor.panchenko@tul.cz>
2024-10-24 13:28:02 +0300

    generator zakladní

--- IS projekty/program004-generator  /obj/Debug/net8.0/program004-generator.AssemblyInfo.cs
+++ IS projekty/program004-generator  /obj/Debug/net8.0/program004-generator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e4531d37acd08ab6d9b2d663a824ef9df046c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb69717a1c1dfaee861879cfeef36551322f0cdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program004-generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
